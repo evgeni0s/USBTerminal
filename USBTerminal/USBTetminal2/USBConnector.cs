4002e6a0ba1a4a2f94aa1b680e122b1ad4f03c5e
evgeni0s <evgeni0s@yandex.ru>
2014-04-01 23:02:22 +0300

    Second test commit from home

--- USBTerminal/USBTetminal2/USBConnector.cs
+++ USBTerminal/USBTetminal2/USBConnector.cs
@@ -38,6 +38,7 @@ namespace USBTetminal2
         private Settings settings = Settings.Default;
         #endregion
 
+        //Second TEST commit
         //Test commit!
         public USBConnector()
         {
