4923152b06861cff64b87b549a84271fdcefa327
e.gynkov <e.gynkov@samsung.com>
2014-04-01 18:18:47 +0300

    first commit

---
+++ USBTetminal2/Controls/Legend/Legend.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace USBTetminal2.Controls.Legend
+{
+
+        public class Legend
+        {
+            public string Name { get; set; }
+            public bool IsChecked { get; set; }
+            public SolidColorBrush Brush { get; set; }
+
+            public Legend(string name, bool isChecked, SolidColorBrush brush)
+            {
+                this.Name = name;
+                this.IsChecked = isChecked;
+                this.Brush = brush;
+            }
+        }
+}
---
+++ USBTetminal2/Graphs/CustomLineGraph.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Research.DynamicDataDisplay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USBTetminal2.Graphs
+{
+    public class CustomLineGraph: ChartPlotter
+    {
+        CustomLineGraph()
+        {
+ 
+        }
+    }
+}
